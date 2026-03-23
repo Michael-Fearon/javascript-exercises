b7a7cc9d9836957f86fdf6315a9a421ecb1a50cf
Michael Fearon <michaelfearon96@gmail.com>
2026-03-23 14:30:56 +1300

    repeatStrings 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, repeatN) {
+    let i = 0;
+    let outputString = "";
 
+    while (i < repeatN){
+        outputString += (string);
+        i ++;
+    }
+    return outputString;
 };
 
 // Do not edit below this line

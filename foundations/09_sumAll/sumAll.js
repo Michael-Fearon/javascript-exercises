fa4b5309ccfd0390603cfce627ed4daf57d62425
Michael Fearon <michaelfearon96@gmail.com>
2026-03-23 17:17:25 +1300

    v1

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(x, y) {
-    return x + y;
+    let range = (max(x,y)-min(x,y));
+    let sum = 0;
+    for (let i = 0; i < range; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

e25e49aa82e36d6e522466306ff9685f5c017ec4
Michael Fearon <michaelfearon96@gmail.com>
2026-03-23 17:09:20 +1300

    removeFromArray 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valToRemove) {
+    let newArray = [];
+    let len = array.length;
+    let i = 0;
+    console.log('Removing: ', valToRemove);
+    while (i < len){
+        if (array[i] != valToRemove){
+            console.log(array[i])
+            newArray.push(array[i]);
+            i++;
+        } else {
+            i++;
+            continue;
+            
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line

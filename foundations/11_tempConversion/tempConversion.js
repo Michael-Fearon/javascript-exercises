d302eb0a46aa8b7c80e90059dc127317b8244a03
Michael Fearon <michaelfearon96@gmail.com>
2026-03-24 15:48:35 +1300

    11 tempConversion Tests Passed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  c = round((f - 32) / (9/5));
+  console.log(f, c);
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  f = round(c * 9/5 + 32);
+  console.log(c, f);
+  return f;
+};
+
+const round = function(input) {
+  return Math.round(input * 10) / 10
 };
 
 // Do not edit below this line

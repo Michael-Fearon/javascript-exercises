8fe29f0927a6d1e1d0c26b3d93873135fd81c6ea
Michael Fearon <michaelfearon96@gmail.com>
2026-03-24 16:36:01 +1300

    12 calculator Tests Passed!

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
+const add = function(...inputs) {
+  let total = 0;
+  for (let i =0; i < inputs.length; i++){
+    total += inputs[i];
+  }
+	return total;
+};
+
+const subtract = function(...inputs) {
+  let total = inputs[0] - inputs[1];
+	return total;
+};
+
+const sum = function(array) {
+  let total = 0;
+  for (let i =0; i < array.length; i++){
+    num = array[i];
+    console.log('num', num)
+    total += num;
+  }
+	return total;
+};
+
+const multiply = function(array) {
+  let total = 0;
+  for (let i =0; i < array.length; i++){
+    if (i===0){
+      total+= array[i];
+    } else {
+      total *= array[i];
+    }
+  }
+	return total;
+};
+
+const power = function(base, exponent) {
+  let total = base;
+  for (let i=1; i < exponent; i++){
+    total *= base;
+  }
+  return total;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(n) {
+  let total = 1;
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  for (let i=1; i<=n; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line

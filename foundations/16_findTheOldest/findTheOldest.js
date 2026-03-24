810ab7ee62b6a73e52e950289da2b276f7da45cc
Michael Fearon <michaelfearon96@gmail.com>
2026-03-24 19:24:35 +1300

    findTheOldest Passed! (Using 2 separate loops)

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,11 +1,22 @@
-const findTheOldest = function(array) {
-    data = {};
-    for (let i=0; i < array.length; i++){
-        entry = array[i];
-        age = entry['yearOfDeath']-entry['yearOfBirth'];
+const findTheOldest = function(people) {
+    let max_age = 0;
+    let data = people;
 
+    for (let i=0; i < people.length; i++){
+        entry = data[i];
+        let deathYear = entry.yearOfDeath || new Date().getFullYear();
+        let birthYear = entry.yearOfBirth;
+
+        let age = deathYear - birthYear;
+        max_age = Math.max(age, max_age);
+        data[i]['age'] = (age);
     }
 
+    for (let i=0; i < data.length; i++){
+        if(data[i]['age'] === max_age){
+            return (data[i]);
+        }
+    }
 };
 
 // Do not edit below this line

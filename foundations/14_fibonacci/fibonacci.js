07bf2d41f65dc1824835691996fa39ada8cc693b
Michael Fearon <michaelfearon96@gmail.com>
2026-03-24 17:14:53 +1300

    Slightly hacky, fibonacci sequence tests passed!

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    sequence = [0,1]
+    if (input === 0 || input === "0"){
+        return 0;
+    } else if (input < 0){
+        return "OOPS";
+    }
 
+    for (let i=1; i!=input; i++){
+        n = sequence[i-1] + sequence[i];
+        sequence.push(n);
+        console.log(sequence, input);
+    }
+
+    return sequence[input];
 };
 
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...
 // Do not edit below this line
 module.exports = fibonacci;

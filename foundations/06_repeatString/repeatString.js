7d2d405f935e328dc1545f4dfb3f86f9bf5763e1
Michael Fearon <michaelfearon96@gmail.com>
2026-03-24 13:26:04 +1300

    06 repeatString tests passed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,9 @@
 const repeatString = function(string, repeatN) {
     let i = 0;
     let outputString = "";
+    if (repeatN < 0){
+        return "ERROR";
+    }
 
     while (i < repeatN){
         outputString += (string);

27e567588e5ab1d9d6922def9e0f9454e6487028
Michael Fearon <michaelfearon96@gmail.com>
2026-03-24 16:50:30 +1300

    13 Palindromes Tests Passed!

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let text = string.replace(/\W/ig, "").toLowerCase();
+    let arr = text.split('');
+    let reversed = arr.reverse().join('');
+    console.log('forward', text);
+    console.log('reversed', reversed);
+
+    if (text === reversed){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 

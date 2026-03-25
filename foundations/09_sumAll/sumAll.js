9a2c7e69f8840b5b3ebcd178c900fbe250da816a
Michael Fearon <michaelfearon96@gmail.com>
2026-03-25 14:02:13 +1300

    Tidying up code

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,19 +1,14 @@
 const sumAll = function(x, y) {
     let max = Math.max(x, y);
     let min = Math.min(x, y);
-    let total = (x+y);
-    let diff = (Math.max(x,y)- Math.min(x,y));
 
-    if (min < 0 || Number.isInteger(x) === false || Number.isInteger(y) === false || total % 1 != 0){
+    if (min < 0 || Number.isInteger(x) === false || Number.isInteger(y) === false || (min+max) % 1 != 0){
         return 'ERROR';
     }
 
     let sumRange = 0;
-    let i = min;
-    while (i <= max){
+    for (let i = min; i <= max; i++){
         sumRange += i;
-        i++;
-        console.log('sumRange', sumRange);
     }
     return sumRange;
 };
